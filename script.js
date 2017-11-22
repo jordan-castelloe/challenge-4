0ff06e2f46822ad203f144c1500b7bbf8521791e
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-11-22 15:31:36 -0500

    added javascript while loop

---
+++ script.js
@@ -0,0 +1,9 @@
+let i = 1;
+
+while (i%2 != 0 || i%3 != 0 || i%4 != 0 || i%5 != 0 || i%6 != 0 || i%7 != 0 || i%8 != 0 || i%9 != 0 || i%10 != 0){
+    i++;
+}
+
+console.log(i);
+
+//The smallest number that can be divided by each of the numbers from 1 to 10 without any remainder is 2520
